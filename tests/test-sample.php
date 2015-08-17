cacae65f73e5022c46d3249ee97dd063371de67a
hideokamoto <kokkoku214@gmail.com>
2015-08-18 01:37:55 +0900

    Make Simple Tests

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -1,10 +1,12 @@
 <?php
 
-class SampleTest extends WP_UnitTestCase {
+require_once 'ejls-master.php';
+class WPCustomizationTest extends WP_UnitTestCase {
 
-	function test_sample() {
+	function test_null_json_data() {
 		// replace this with some actual testing code
-		$this->assertTrue( true );
+		$null_data = null;
+		$jsonld_output = ejls_make_html($null_data);
+		$this->assertEquals( "<script type='application/ld+json'></script>", $jsonld_output );
 	}
 }
-
